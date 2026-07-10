7f257ebc22dc8c741cfedcf01fe31b7681f56e15
Sam <lunar_sam@protonmail.com>
2026-07-10 15:24:48 -0400

    what?

--- libspesscomputers/spawn_linux.cpp
+++ libspesscomputers/spawn_linux.cpp
@@ -1,4 +1,16 @@
 #ifdef __linux__
 #include "spawn.hpp"
 
+#include <spawn.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <strings.h>
+#include <ctime>
+int OpenSpesscore(std::string path, std::vector<char *> args, int * spess_pid) {
+    pid_t pid = 0;
+    int ok = posix_spawn(&pid, path.c_str(), NULL, NULL, args.data(), environ);
+    *spess_pid = pid;
+    return ok;
+}
+
 #endif
--- libspesscomputers/spawn_win.cpp
+++ libspesscomputers/spawn_win.cpp
@@ -1,6 +1,6 @@
 #ifdef __WIN32__
 #error "Windows process spawning not yet implemented."
-#include <spawn.hpp>
+#include "spawn.hpp"
 
 int OpenSpesscore(std::string path, std::vector<char *> args) {
     return 0;
