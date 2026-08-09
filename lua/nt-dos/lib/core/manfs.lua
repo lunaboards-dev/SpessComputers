8fde44f608df6ad1abf1d19d297bf3e115546df9
Sam <lunar_sam@protonmail.com>
2026-08-09 18:17:23 -0400

    start on this shit

--- lua/nt-dos/lib/core/manfs.lua
+++ lua/nt-dos/lib/core/manfs.lua
@@ -1,5 +1,39 @@
 local fs = {}
 
+function fs:open(path, flags)
 
+end
+
+function fs:pstat(path)
+
+end
+
+function fs:rstat(parent, entry)
+
+end
+
+function fs:opendir(path)
+
+end
+
+function fs:used()
+
+end
+
+function fs:size()
+
+end
+
+function fs:readlink(path)
+
+end
+
+function fs:mklink(path, target)
+
+end
+
+function fs:mkdir(path)
+
+end
 
 return fs
